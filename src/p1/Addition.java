5f7474ccb55e02a5e38cd120d66fc8862dd0c939
Dawid <kotwickidawid@gmail.com>
2015-11-24 12:32:19 +0000

    Branch for W

--- src/p1/Addition.java
+++ src/p1/Addition.java
@@ -1,12 +1,7 @@
 package p1;
 
 import javax.swing.JTextField;
-
-import java.awt.EventQueue;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
+import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -17,14 +12,15 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-
 import net.miginfocom.swing.MigLayout;
 
 public class Addition extends JPanel {
-	
+
     public Addition() {
-   
-    	
-      }
+        JPanel Panel = new JPanel(new MigLayout());
+        JLabel lbl1 = new JLabel("How many values do you want to add ?");
+        Panel.add(lbl1);
+        Panel.setVisible(true);
+    }
 
 }
