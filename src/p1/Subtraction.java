d9ecd6d83ee54dc306ab5b370a56451f7e285881
liamkeane93 <g00287233@gmit.ie>
2015-11-30 16:54:20 +0000

    Subtraction gui progress

--- src/p1/Subtraction.java
+++ src/p1/Subtraction.java
@@ -1,7 +1,36 @@
 package p1;
 
-import javax.swing.JPanel;
+import net.miginfocom.swing.MigLayout;
+
+import javax.swing.*;
 
 public class Subtraction extends JPanel {
+
+    public Subtraction(){
+        MigLayout mg = new MigLayout("");
+        setLayout(mg);
+        setSize(400, 400);
+
+        JLabel title = new JLabel("Math check subtraction lets you visualize how subtraction is done!");
+        JLabel var1Label = new JLabel("Enter Value1");
+        JTextField value1 = new JTextField(8);
+        JLabel var2Label = new JLabel("Enter Value2");
+        JTextField value2 = new JTextField(8);
+
+        add(title, "split2,right, wrap");
+
+        add(var1Label,"split 2, sg a");
+        add(value1,"wrap");
+
+        add(var2Label,"split 2, sg a");
+        add(value2,"wrap");
+
+
+
+
+
+
+
+    }
 
 }
